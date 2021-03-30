4a5d7392d7413fce7bd12da26cce025e82432ef0
Eric Burns <ericjosephburns@gmail.com>
2021-03-29 22:10:11 -0500

    Adds placeholder questions

---
+++ config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'placeholderQuestions' => array(
+        "Where do vegans get their protein?",
+        "Crop deaths tho?",
+        "If everyone became vegan, what would we do with all the animals?",
+        "Where do you get your iron?",
+        "Isn't veganism actually worse for the planet?",
+        "Why are vegans so weak?",
+        "Bro, don't plants feel pain too?",
+        "If we aren't supposed to eat meat, why do we have canines?"
+    )
+];
